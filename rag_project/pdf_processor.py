ab30e9ca7745ec8a99c817505225d2f6f757d301
Rishi Shahu <rishishahu11@gmail.com>
2026-06-17 22:08:29 +0530

    Add files via upload

---
+++ rag_project/pdf_processor.py
@@ -0,0 +1,27 @@
+from pypdf import PdfReader
+
+def extract_text(pdf_path):
+    reader = PdfReader(pdf_path)
+
+    text = ""
+
+    for page in reader.pages:
+        page_text = page.extract_text()
+
+        if page_text:
+            text += page_text + "\n"
+
+    return text
+
+
+def create_chunks(text, chunk_size=500):
+
+    words = text.split()
+
+    chunks = []
+
+    for i in range(0, len(words), chunk_size):
+        chunk = " ".join(words[i:i + chunk_size])
+        chunks.append(chunk)
+
+    return chunks
