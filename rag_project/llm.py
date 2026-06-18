0da6824e866d74166ee849a4bf635f1ce029f8d7
Rishi Shahu <rishishahu11@gmail.com>
2026-06-18 12:23:49 +0530

    Add files via upload

---
+++ rag_project/llm.py
@@ -0,0 +1,41 @@
+from groq import Groq
+
+client = Groq(
+    api_key="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"
+)
+
+
+def generate_answer(
+    context,
+    question
+):
+
+    prompt = f"""
+You are a PDF Question Answering Assistant.
+
+Answer ONLY from the context.
+
+If the answer is not found in the context,
+reply exactly:
+
+I cannot find this information in the document.
+
+Context:
+{context}
+
+Question:
+{question}
+"""
+
+    response = client.chat.completions.create(
+        model="llama-3.3-70b-versatile",
+        messages=[
+            {
+                "role": "user",
+                "content": prompt
+            }
+        ],
+        temperature=0
+    )
+
+    return response.choices[0].message.content
